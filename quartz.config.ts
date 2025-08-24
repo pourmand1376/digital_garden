f32481bec6ef2c64cb125a84ee86a35503092ce1
Amir Pourmand <pourmand1376@gmail.com>
2025-08-25 01:12:36 +0330

    Update analytics provider to GoatCounter with specified website ID in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "goatcounter",
+      websiteId: "aprd-notes",
     },
     locale: "fa-IR",
     baseUrl: "notes.aprd.ir",
