6307939ffedda2968f946212365e6afdd6c2f39a
Amir Pourmand <pourmand1376@gmail.com>
2025-08-25 00:50:23 +0330

    Add GoatCounter script for analytics tracking in Head component

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -97,6 +97,11 @@ export default (() => {
             return resource
           }
         })}
+        <script
+          data-goatcounter="https://aprd-notes.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+        ></script>
       </head>
     )
   }
