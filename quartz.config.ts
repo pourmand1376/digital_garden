88e72d818305ea04d8cab6726db91bbc8a006dbd
Amir Pourmand <pourmand1376@gmail.com>
2025-09-09 14:43:43 +0330

    update quartz layout

--- quartz.config.ts
+++ quartz.config.ts
@@ -70,7 +70,11 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ 
+        markdownLinkResolution: "shortest", prettyLinks: true, 
+        externalLinkIcon: true, 
+        openLinksInNewTab: true,
+       }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.HardLineBreaks(),
